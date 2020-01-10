674d55bd02a896b35b8fc1b91cd72fc7ca65972e
liuxiaoxuan <liuxiaoxuan@sequoiadb.com>
2020-01-10 16:18:52 +0800

    调整mariadb用例支持并发执行

--- testcase/mariadb-10.4.6/mysql-test-run.pl
+++ testcase/mariadb-10.4.6/mysql-test-run.pl
@@ -3319,6 +3319,20 @@ sub mysql_install_db {
   # Create directories mysql and test
   mkpath("$install_datadir/mysql");
 
+  # liuxiaoxuan: Create directories for each parallel when parallel > 1
+  mtr_tofile($bootstrap_sql_file,
+             "set global sequoiadb_auto_partition=on;\n");
+  for my $child_num (1..$opt_parallel)
+  {
+     mkpath("$install_datadir/test_parallel_$child_num");
+     #zhaoyu: Create table for sequoiadb to reserved database file
+     mtr_tofile($bootstrap_sql_file,
+             "CREATE TABLE test_parallel_$child_num.reserved(a int primary key)engine=sequoiadb;\n");
+  }
+  mtr_tofile($bootstrap_sql_file,
+             "set global sequoiadb_auto_partition=off;\n");
+
+
   my $realtime= gettimeofday();
   if ( My::SafeProcess->run
        (
@@ -5655,7 +5669,11 @@ sub start_mysqltest ($) {
   mtr_add_arg($args, "--mark-progress")
     if $opt_mark_progress;
 
-  mtr_add_arg($args, "--database=test");
+  # liuxiaoxuan: Use unique database for each parallel
+  my $parallel_thread = $tinfo->{worker};
+  $parallel_thread = 1 if $opt_parallel <= 1;
+  my $current_parallel_database = "test_parallel_$parallel_thread";
+  mtr_add_arg($args, "--database=$current_parallel_database");
 
   if ( $opt_ps_protocol )
   {
