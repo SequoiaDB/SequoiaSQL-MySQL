1aad463d9cdffddf0913b7c3a7411e9bdec1a400
niezhibiao <niezhibiao@sequoiadb.com>
2021-08-26 17:45:26 +0800

    SEQUOIASQLMAINSTREAM-1105 解决 MariaDB 分区表没有限制 YEAR(2)类型字段的问题.

--- src/sequoiadb/handler/ha_sdb_part.cc
+++ src/sequoiadb/handler/ha_sdb_part.cc
@@ -2406,6 +2406,12 @@ int ha_sdb_part::create(const char *name, TABLE *form,
     for (Field **fields = form->field; *fields; fields++) {
       Field *field = *fields;
 
+      if (field->type() == MYSQL_TYPE_YEAR && field->field_length != 4) {
+        rc = ER_INVALID_YEAR_COLUMN_LENGTH;
+        my_printf_error(rc, "Supports only YEAR or YEAR(4) column", MYF(0));
+        goto error;
+      }
+
       if (field->key_length() >= SDB_FIELD_MAX_LEN) {
         my_error(ER_TOO_BIG_FIELDLENGTH, MYF(0), sdb_field_name(field),
                  static_cast<ulong>(SDB_FIELD_MAX_LEN));
